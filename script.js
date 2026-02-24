07b1a4861fe7f04251053c64f24ac5b6481ac291
Rajasree billakurthi <rajasreebillakurthi@gmail.com>
2026-02-24 11:36:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,16 @@ const prices = {
     Deluxe: 5000
 };
 
-function openBooking() {
-    document.getElementById("bookingModal").style.display = "flex";
+function goToDetails() {
+    document.getElementById("details").scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToBooking() {
+    document.querySelector(".booking-box").scrollIntoView({ behavior: "smooth" });
+
     const today = new Date().toISOString().split("T")[0];
     document.getElementById("date").min = today;
     document.getElementById("date").value = today;
-}
-
-function closeBooking() {
-    document.getElementById("bookingModal").style.display = "none";
 }
 
 function bookNow() {
@@ -23,7 +24,7 @@ function bookNow() {
     const roomType = document.getElementById("roomType").value;
 
     if (name === "" || roomType === "") {
-        alert("Please fill all details");
+        alert("Fill all details");
         return;
     }
 
@@ -34,15 +35,4 @@ function bookNow() {
     document.getElementById("totalAmount").textContent = totalAmount;
 
     alert("Booking Successful ✅");
-    closeBooking();
 }
-
-/* SCROLL EFFECT */
-window.addEventListener("scroll", () => {
-    const heroText = document.getElementById("heroText");
-    if (window.scrollY > 100) {
-        heroText.classList.add("sticky");
-    } else {
-        heroText.classList.remove("sticky");
-    }
-});
